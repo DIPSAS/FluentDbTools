571f4b9751380522a522a6a58eb60ed4e3903c7d
Hans Erik Heggem <heh@dips.no>
2019-02-18 11:37:47 +0100

    Added support for overriding functions in DefaultDbConfig

--- src/FluentDbTools/Extensions/FluentDbTools.Extensions.MSDependencyInjection/DefaultConfigs/DefaultDbConfig.cs
+++ src/FluentDbTools/Extensions/FluentDbTools.Extensions.MSDependencyInjection/DefaultConfigs/DefaultDbConfig.cs
@@ -13,98 +13,98 @@ namespace FluentDbTools.Extensions.MSDependencyInjection.DefaultConfigs
         }
 
         private SupportedDatabaseTypes? DbTypeField;
-        public SupportedDatabaseTypes DbType
+        public virtual SupportedDatabaseTypes DbType
         {
             get => DbTypeField ?? Configuration.GetDbType();
             set => DbTypeField = value;
         }
         
         private string UserField;
-        public string User
+        public virtual string User
         {
             get => UserField ?? Configuration.GetDbUser();
             set => UserField = value;
         }
         
         private string PasswordField;
-        public string Password
+        public virtual string Password
         {
             get => PasswordField ?? Configuration.GetDbPassword();
             set => PasswordField = value;
         }
         
         private string AdminUserField;
-        public string AdminUser
+        public virtual string AdminUser
         {
             get => AdminUserField ?? Configuration.GetDbAdminUser();
             set => AdminUserField = value;
         }
         
         private string AdminPasswordField;
-        public string AdminPassword
+        public virtual string AdminPassword
         {
             get => AdminPasswordField ?? Configuration.GetDbAdminPassword();
             set => AdminPasswordField = value;
         }
 
         private string HostnameField;
-        public string Hostname
+        public virtual string Hostname
         {
             get => HostnameField ?? Configuration.GetDbHostname();
             set => HostnameField = value;
         }
 
         private string PortField;
-        public string Port
+        public virtual string Port
         {
             get => PortField ?? Configuration.GetDbPort();
             set => PortField = value;
         }
 
         private string DatabaseConnectionNameField;
-        public string DatabaseConnectionName
+        public virtual string DatabaseConnectionName
         {
             get => DatabaseConnectionNameField ?? Configuration.GetDbConnectionName();
             set => DatabaseConnectionNameField = value;
         }
 
         private bool? PoolingField;
-        public bool Pooling
+        public virtual bool Pooling
         {
             get => PoolingField ?? Configuration.GetDbPooling();
             set => PoolingField = value;
         }
 
         private string SchemaField;
-        public string Schema
+        public virtual string Schema
         {
             get => SchemaField ?? Configuration.GetDbSchema();
             set => SchemaField = value;
         }
 
         private string SchemaPasswordField;
-        public string SchemaPassword
+        public virtual string SchemaPassword
         {
             get => SchemaPasswordField ?? Configuration.GetDbSchemaPassword();
             set => SchemaPasswordField = value;
         }
 
         private string TablespaceField;
-        public string DefaultTablespace
+        public virtual string DefaultTablespace
         {
             get => TablespaceField ?? Configuration.GetDbDefaultTablespace();
             set => TablespaceField = value;
         }
 
         private string TempTablespaceField;
-        public string TempTablespace
+        public virtual string TempTablespace
         {
             get => TempTablespaceField ?? Configuration.GetDbTempTablespace();
             set => TempTablespaceField = value;
         }
 
         private string ConnectionStringTemplateField;
-        public string ConnectionStringTemplate
+        public virtual string ConnectionStringTemplate
         {
             get => ConnectionStringTemplateField ?? Configuration.GetDbConnectionStringTemplate();
             set => ConnectionStringTemplateField = value;
