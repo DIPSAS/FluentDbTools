e180e780c82a9587fa0e70d615d882e3d92420ae
Arild Håkon Olse <aho@dips.no>
2019-04-09 11:42:14 +0200

    - Added support for scanning for VersionTableMetaData  - Added extension method ConfigureOracleTnsAdminPath to configure TNS_ADMIN path

--- src/FluentDbTools/Extensions/FluentDbTools.Extensions.Migration/ConfigureWithMigrationExtensions.cs
+++ src/FluentDbTools/Extensions/FluentDbTools.Extensions.Migration/ConfigureWithMigrationExtensions.cs
@@ -1,10 +1,13 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using FluentDbTools.Extensions.MSDependencyInjection;
 using FluentDbTools.Extensions.MSDependencyInjection.DefaultConfigs;
 using FluentDbTools.Common.Abstractions;
 using FluentDbTools.Extensions.Migration.DefaultConfigs;
 using FluentDbTools.Migration.Abstractions;
+using FluentMigrator.Runner.VersionTableInfo;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 
@@ -12,14 +15,52 @@ namespace FluentDbTools.Extensions.Migration
 {
     public static class ConfigureWithMigrationExtensions
     {
-        public static IServiceCollection ConfigureWithMigration(this IServiceCollection serviceCollection, IEnumerable<Assembly> assembliesWithMigrationModels)
+        public static IServiceCollection ConfigureWithMigrationAndScanForVersionTable(
+            this IServiceCollection serviceCollection, 
+            IEnumerable<Assembly> assembliesWithMigrationModels)
         {
+            var assembliesWithMigrationModelArray = assembliesWithMigrationModels.ToArray();
+
+            return serviceCollection
+                .ConfigureWithMigration(assembliesWithMigrationModelArray)
+                .ConfigureVersionTableMetaData(assembliesWithMigrationModelArray);
+        }
+
+        public static IServiceCollection ConfigureWithMigration(
+            this IServiceCollection serviceCollection, 
+            IEnumerable<Assembly> assembliesWithMigrationModels)
+        {
+            var assembliesWithMigrationModelArray = assembliesWithMigrationModels.ToArray();
             return serviceCollection
                 .Register(FluentDbTools.Migration.Oracle.ServiceRegistration.Register)
                 .Register(FluentDbTools.Migration.Postgres.ServiceRegistration.Register)
                 .AddDefaultDbMigrationConfig()
                 .ConfigureWithMigrationAssemblies(FluentDbTools.Migration.ServiceRegistration.Register,
-                    assembliesWithMigrationModels);
+                    assembliesWithMigrationModelArray);
+        }
+
+        internal static IServiceCollection ConfigureVersionTableMetaData(
+            this IServiceCollection serviceCollection,
+            IEnumerable<Assembly> assembliesWithMigrationModels)
+        {
+            var serviceType = typeof(IVersionTableMetaData);
+            foreach (var assembliesWithMigrationModel in assembliesWithMigrationModels)
+            {
+                var implementationType = assembliesWithMigrationModel
+                    .GetTypes()
+                    .FirstOrDefault(x => !x.IsInterface && !x.IsAbstract && serviceType.IsAssignableFrom(x));
+
+                if (implementationType == null)
+                {
+                    continue;
+                }
+
+                serviceCollection.Remove<IVersionTableMetaData>();
+                serviceCollection.AddScoped(serviceType, implementationType);
+                return serviceCollection;
+            }
+
+            return serviceCollection;
         }
 
         public static IServiceCollection AddDefaultDbMigrationConfig(this IServiceCollection serviceCollection)
--- src/FluentDbTools/Tests/Test.FluentDbTools.Migration/MigrationTests.cs
+++ src/FluentDbTools/Tests/Test.FluentDbTools.Migration/MigrationTests.cs
@@ -1,7 +1,11 @@
 using System;
+using System.Data;
+using System.Linq;
+using Dapper;
 using FluentDbTools.Common.Abstractions;
 using Example.FluentDbTools.Config;
 using Example.FluentDbTools.Migration;
+using Example.FluentDbTools.MigrationWithVersionTable;
 using FluentAssertions;
 using FluentDbTools.DbProviders;
 using FluentDbTools.Extensions.DbProvider;
@@ -10,10 +14,12 @@ using FluentDbTools.Extensions.Migration.DefaultConfigs;
 using FluentDbTools.Extensions.MSDependencyInjection;
 using FluentDbTools.Extensions.MSDependencyInjection.DefaultConfigs;
 using FluentDbTools.Extensions.MSDependencyInjection.Oracle;
+using FluentDbTools.Extensions.MSDependencyInjection.Postgres;
 using FluentDbTools.Migration;
 using FluentDbTools.Migration.Abstractions;
 using TestUtilities.FluentDbTools;
 using FluentMigrator.Runner;
+using FluentMigrator.Runner.Versioning;
 using FluentMigrator.Runner.VersionTableInfo;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -47,7 +53,7 @@ namespace Test.FluentDbTools.Migration
 
         [Theory]
         [MemberData(nameof(TestParameters.DbParameters), MemberType = typeof(TestParameters))]
-        public void MigrationWithExecutorExtension_Success(SupportedDatabaseTypes databaseType)
+        public void MigrationWithExecutorExtensionWithoutVersionInfo_Success(SupportedDatabaseTypes databaseType)
         {
             var inMemoryOverrideConfig = OverrideConfig.GetInMemoryOverrideConfig(databaseType, OverrideConfig.NewRandomSchema);
             var configuration = new ConfigurationBuilder()
@@ -58,16 +64,60 @@ namespace Test.FluentDbTools.Migration
             var serviceProvider = new ServiceCollection()
                 .AddScoped<IConfiguration>(provider => configuration)
                 .AddDefaultDbMigrationConfig()
+                .AddOracleDbProvider()
+                .AddPostgresDbProvider()
                 .BuildServiceProvider();
 
             var dbConfig = serviceProvider.GetService<IDbMigrationConfig>();
 
             var runner = dbConfig.GetMigrationRunner(MigrationBuilder.MigrationAssemblies);
 
-            runner.MigrateUp();
-
-            dbConfig.DropSchema(MigrationBuilder.MigrationAssemblies);
+            try
+            {
+                runner.MigrateUp();
+                VerifyExpectedVersionInfoTable(serviceProvider, nameof(VersionInfo));
+            }
+            finally
+            {
+                dbConfig.DropSchema(MigrationBuilder.MigrationAssemblies);
+            }
         }
+
+        [Theory]
+        [MemberData(nameof(TestParameters.DbParameters), MemberType = typeof(TestParameters))]
+        public void MigrationWithExecutorExtensionWithVersionInfo_Success(SupportedDatabaseTypes databaseType)
+        {
+            var inMemoryOverrideConfig = OverrideConfig.GetInMemoryOverrideConfig(databaseType, OverrideConfig.NewRandomSchema);
+            var configuration = new ConfigurationBuilder()
+                .AddDbToolsExampleConfiguration(databaseType)
+                .AddInMemoryCollection(inMemoryOverrideConfig)
+                .Build();
+
+            var serviceProvider = new ServiceCollection()
+                .AddScoped<IConfiguration>(provider => configuration)
+                .AddDefaultDbMigrationConfig()
+                .AddOracleDbProvider()
+                .AddPostgresDbProvider()
+                .BuildServiceProvider();
+
+            var dbConfig = serviceProvider.GetService<IDbMigrationConfig>();
+            
+            var assemblies = MigrationBuilder.MigrationAssemblies.ToList();
+            assemblies.Add(typeof(ExampleVersionTable).Assembly);
+
+            var runner = dbConfig.GetMigrationRunner(assemblies);
+
+            try
+            {
+                runner.MigrateUp();
+                VerifyExpectedVersionInfoTable(serviceProvider, nameof(ExampleVersionTable));
+            }
+            finally
+            {
+                dbConfig.DropSchema(assemblies);
+            }
+        }
+
 
         [Fact]
         public void OracleMigration_WhenDataSourceIsValidEzConnect_Success()
@@ -173,5 +223,16 @@ namespace Test.FluentDbTools.Migration
             }
         }
 
+        private static void VerifyExpectedVersionInfoTable(IServiceProvider serviceProvider, string versionTable)
+        {
+            using (var connection = serviceProvider
+                .CreateScope()
+                .ServiceProvider
+                .GetRequiredService<IDbConnection>())
+            {
+                var version = connection.Query<ExampleVersionTable>($"select * from {versionTable}");
+                version.Should().NotBeNull();
+            }
+        }
     }
 }
