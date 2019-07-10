8d41ef4506497559513411c84b0521bcaf4edf88
Arild Håkon Olsen <aho@dips.no>
2019-07-10 16:31:10 +0200

    Improved Migration-logging configuration

--- src/FluentDbTools/Extensions/FluentDbTools.Extensions.MSDependencyInjection/ConfigurationExtensions.cs
+++ src/FluentDbTools/Extensions/FluentDbTools.Extensions.MSDependencyInjection/ConfigurationExtensions.cs
@@ -7,38 +7,100 @@ namespace FluentDbTools.Extensions.MSDependencyInjection
     {
         public static string GetConfigValue(this IConfiguration configuration, params string[] keys)
         {
+            if (configuration == null)
+            {
+                return null;
+            }
+
             foreach (var key in keys)
             {
-                var str = configuration[key];
-                if (!str.IsEmpty())
-                    return str;
+                var value = configuration[key];
+                if (!value.IsEmpty())
+                {
+                    return value;
+                }
             }
+
+            var sections = new[] { "Logging", "database:migration", "database" };
+
+            foreach (var section in sections)
+            {
+                var dbSection = configuration.GetSection(section);
+                if (dbSection == null)
+                {
+                    continue;
+                }
+
+                foreach (var key in keys)
+                {
+                    var value = dbSection[key];
+                    if (!value.IsEmpty())
+                    {
+                        return value;
+                    }
+                }
+            }
+
             return null;
         }
 
+        /// <summary>
+        /// MigrationLogging with Console logging can be enabled from configuration key(s):
+        ///     ConsoleLog = true
+        ///     OR
+        ///     [Logging:Console:LogLevel | Logging:LogLevel:Console] != None
+        ///     When both Logging:Console:LogLevel and Logging:LogLevel:Console is null, Console logging will be enabled.
+        /// </summary>
+        /// <param name="configuration"></param>
+        /// <returns></returns>
         public static bool IsConsoleLogEnabled(this IConfiguration configuration)
         {
-            return configuration.GetConfigValue("Logging:Console:LogLevel", "Logging:LogLevel:Console").WithDefault("Information") != "None";
+            var consoleLogEnabled = configuration.GetConfigValue("ConsoleLog").WithDefault("false") == "true";
+            return consoleLogEnabled || configuration.GetConfigValue("Logging:Console:LogLevel", "Logging:LogLevel:Console").WithDefault("Information") != "None";
         }
 
+        /// <summary>
+        /// MigrationLogging with Sql logging can be enabled from configuration key(s):
+        ///     [Logging:Migration:ShowSql | Logging:LogShowSql] = true
+        /// </summary>
+        /// <param name="configuration"></param>
+        /// <returns></returns>
         public static bool IsMigrationLogShowSqlEnabled(this IConfiguration configuration)
         {
-            return configuration.GetConfigValue("Logging:Migration:ShowSql").WithDefault("false") != "false";
+            return configuration.GetConfigValue("Logging:Migration:ShowSql" ,"LogShowSql").WithDefault("false") != "false";
         }
 
+        /// <summary>
+        /// MigrationLogging with File logging can be enabled from configuration key(s):
+        ///     [Logging:Migration:File | Logging:LogFile] 
+        /// </summary>
+        /// <param name="configuration"></param>
+        /// <returns></returns>
         public static string GetMigrationLogFile(this IConfiguration configuration)
         {
-            return configuration.GetConfigValue("Logging:Migration:File");
+            return configuration.GetConfigValue("Logging:Migration:File", "LogFile");
         }
 
+        /// <summary>
+        /// MigrationLogging with File logging can be enabled from configuration key(s):
+        ///     [Logging:Migration:File | Logging:LogFile] 
+        /// </summary>
+        /// <param name="configuration"></param>
+        /// <returns></returns>
         public static bool IsMigrationLogFileEnabled(this IConfiguration configuration)
         {
             return !configuration.GetMigrationLogFile().IsEmpty();
         }
 
+        /// <summary>
+        /// MigrationLogging with ElapsedTime can be enabled from configuration key(s):
+        ///     [Logging:Migration:ShowElapsedTime | Logging:LogShowElapsedTime] = true
+        /// </summary>
+        /// <param name="configuration"></param>
+        /// <returns></returns>
         public static bool IsMigrationLogShowElapsedTimeEnabled(this IConfiguration configuration)
         {
-            return configuration.GetConfigValue("Logging:Migration:ShowElapsedTime").WithDefault("false") != "false";
+            return configuration.GetConfigValue("Logging:Migration:ShowElapsedTime", "LogShowElapsedTime").WithDefault("false") != "false";
         }
     }
 }
--- src/FluentDbTools/Implementations/FluentDbTools.Migration/FluentMigrationLoggingExtensions.cs
+++ src/FluentDbTools/Implementations/FluentDbTools.Migration/FluentMigrationLoggingExtensions.cs
@@ -1,8 +1,5 @@
-﻿
-using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
 using FluentDbTools.Extensions.MSDependencyInjection;
 using FluentMigrator.Runner;
 using FluentMigrator.Runner.Initialization;
@@ -14,86 +11,101 @@ using Microsoft.Extensions.Options;
 
 namespace FluentDbTools.Migration
 {
-  public static class FluentMigrationLoggingExtensions
-  {
-    public static ILoggingBuilder AddFluentMigratorConsole(
-      this ILoggingBuilder loggingBuilder,
-      IConfiguration configuration)
+    public static class FluentMigrationLoggingExtensions
     {
-      configuration = GetConfiguration(loggingBuilder, configuration);
-      if (configuration == null || !configuration.IsConsoleLogEnabled())
-        return loggingBuilder;
-      loggingBuilder.ClearProviders();
-      return loggingBuilder.AddFluentMigratorConsole(new FluentMigratorLoggerOptions
-      {
-        ShowSql = configuration.IsMigrationLogShowSqlEnabled(),
-        ShowElapsedTime = configuration.IsMigrationLogShowElapsedTimeEnabled()
-      });
-    }
+        public static ILoggingBuilder AddFluentMigratorConsoleLogger(
+          this ILoggingBuilder loggingBuilder,
+          IConfiguration configuration = null)
+        {
+            configuration = GetConfiguration(loggingBuilder, configuration);
+            if (configuration == null || !configuration.IsConsoleLogEnabled())
+            {
+                return loggingBuilder;
+            }
+            loggingBuilder.ClearProviders();
 
-    public static ILoggingBuilder AddFluentMigratorConsole(
-      this ILoggingBuilder loggingBuilder,
-      FluentMigratorLoggerOptions options,
-      bool clearProviders = false)
-    {
-      if (clearProviders)
-        loggingBuilder.ClearProviders();
-      return loggingBuilder.AddProvider(new FluentMigratorConsoleLoggerProvider(new OptionsWrapper<FluentMigratorLoggerOptions>(options)));
-    }
+            var options = new FluentMigratorLoggerOptions
+            {
+                ShowSql = configuration.IsMigrationLogShowSqlEnabled(),
+                ShowElapsedTime = configuration.IsMigrationLogShowElapsedTimeEnabled()
+            };
 
-    public static ILoggingBuilder AddFluentMigratorFileLogger(
-      this ILoggingBuilder loggingBuilder,
-      IConfiguration configuration = null)
-    {
-      configuration = GetConfiguration(loggingBuilder, configuration);
-      if (configuration == null || !configuration.IsMigrationLogFileEnabled())
-        return loggingBuilder;
-      var loggingBuilder1 = loggingBuilder;
-      var options = new LogFileFluentMigratorLoggerOptions
-      {
-          ShowSql = configuration.IsMigrationLogShowSqlEnabled(),
-          ShowElapsedTime = configuration.IsMigrationLogShowElapsedTimeEnabled(),
-          OutputFileName = configuration.GetMigrationLogFile()
-      };
-      return loggingBuilder1.AddFluentMigratorFileLogger(options);
-    }
+            loggingBuilder.Services.AddSingleton<IOptions<FluentMigratorLoggerOptions>>(new OptionsWrapper<FluentMigratorLoggerOptions>(options));
 
-    public static ILoggingBuilder AddFluentMigratorFileLogger(
-      this ILoggingBuilder loggingBuilder,
-      LogFileFluentMigratorLoggerOptions options,
-      bool clearProviders = false)
-    {
-      if (clearProviders)
-        loggingBuilder.ClearProviders();
-      var implementation = loggingBuilder.Services.GetFirstServiceDescriptor<IMigrationSourceItem>().GetImplementation<IMigrationSourceItem>((IServiceScope) null);
-      Type type1;
-      if (implementation == null)
-      {
-        type1 = null;
-      }
-      else
-      {
-        var migrationTypeCandidates = implementation.MigrationTypeCandidates;
-        type1 = migrationTypeCandidates != null ? migrationTypeCandidates.FirstOrDefault() : null;
-      }
-      var type2 = type1;
-      AssemblySourceItem assemblySourceItem;
-      if (!(type2 == null))
-        assemblySourceItem = new AssemblySourceItem(type2.Assembly);
-      else
-        assemblySourceItem = new AssemblySourceItem(Array.Empty<Assembly>());
-      var assemblySource = new AssemblySource(new OptionsManager<AssemblySourceOptions>(new OptionsFactory<AssemblySourceOptions>(Enumerable.Empty<IConfigureOptions<AssemblySourceOptions>>(), Enumerable.Empty<IPostConfigureOptions<AssemblySourceOptions>>())), new List<IAssemblyLoadEngine>(), new AssemblySourceItem[1]
-      {
-          assemblySourceItem
-      });
-      return loggingBuilder.AddProvider(new LogFileFluentMigratorLoggerProvider(assemblySource, new OptionsWrapper<LogFileFluentMigratorLoggerOptions>(options)));
-    }
+            return loggingBuilder.AddFluentMigratorConsoleLogger(options);
+        }
 
-    private static IConfiguration GetConfiguration(
-      ILoggingBuilder loggingBuilder,
-      IConfiguration configuration)
-    {
-      return configuration ?? loggingBuilder.Services.GetFirstServiceDescriptor<IConfiguration>().GetImplementation<IConfiguration>(loggingBuilder.Services.BuildServiceProvider().CreateScope());
+        public static ILoggingBuilder AddFluentMigratorConsoleLogger(
+          this ILoggingBuilder loggingBuilder,
+          FluentMigratorLoggerOptions options,
+          bool clearProviders = false)
+        {
+            if (clearProviders)
+            {
+                loggingBuilder.ClearProviders();
+            }
+
+            return loggingBuilder.AddProvider(new FluentMigratorConsoleLoggerProvider(new OptionsWrapper<FluentMigratorLoggerOptions>(options)));
+        }
+
+        public static ILoggingBuilder AddFluentMigratorFileLogger(
+          this ILoggingBuilder loggingBuilder,
+          IConfiguration configuration = null)
+        {
+            configuration = GetConfiguration(loggingBuilder, configuration);
+
+            if (configuration == null || !configuration.IsMigrationLogFileEnabled())
+            {
+                return loggingBuilder;
+            }
+
+            var options = new LogFileFluentMigratorLoggerOptions
+            {
+                ShowSql = configuration.IsMigrationLogShowSqlEnabled(),
+                ShowElapsedTime = configuration.IsMigrationLogShowElapsedTimeEnabled(),
+                OutputFileName = configuration.GetMigrationLogFile()
+            };
+
+            return loggingBuilder.AddFluentMigratorFileLogger(options);
+        }
+
+        public static ILoggingBuilder AddFluentMigratorFileLogger(
+          this ILoggingBuilder loggingBuilder,
+          LogFileFluentMigratorLoggerOptions options,
+          bool clearProviders = false)
+        {
+            if (clearProviders)
+            {
+                loggingBuilder.ClearProviders();
+            }
+
+            var assembly = loggingBuilder.Services
+                                    .GetFirstServiceDescriptor<IMigrationSourceItem>()
+                                    .GetImplementation<IMigrationSourceItem>()
+                                    ?.MigrationTypeCandidates?.FirstOrDefault()?.Assembly;
+
+            var assemblySourceItem = assembly == null
+                ? new AssemblySourceItem()
+                : new AssemblySourceItem(assembly);
+
+            var optionsManager = new OptionsManager<AssemblySourceOptions>(
+                new OptionsFactory<AssemblySourceOptions>(
+                    Enumerable.Empty<IConfigureOptions<AssemblySourceOptions>>(),
+                    Enumerable.Empty<IPostConfigureOptions<AssemblySourceOptions>>()));
+
+            var assemblySource = new AssemblySource(optionsManager, new List<IAssemblyLoadEngine>(), new[] { assemblySourceItem });
+
+            return loggingBuilder.AddProvider(new LogFileFluentMigratorLoggerProvider(assemblySource, new OptionsWrapper<LogFileFluentMigratorLoggerOptions>(options)));
+        }
+
+        private static IConfiguration GetConfiguration(
+          ILoggingBuilder loggingBuilder,
+          IConfiguration configuration)
+        {
+            return configuration ??
+                   loggingBuilder.Services
+                       .GetFirstServiceDescriptor<IConfiguration>()
+                       .GetImplementation<IConfiguration>(loggingBuilder.Services.BuildServiceProvider().CreateScope());
+        }
     }
-  }
 }
