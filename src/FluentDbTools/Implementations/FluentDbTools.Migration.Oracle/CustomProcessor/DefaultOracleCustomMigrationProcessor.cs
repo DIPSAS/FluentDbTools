f55cfc4a86d3d54b31fdda0cd0bf74ee3ad6454b
Arild Håkon Olsen <aho@dips.no>
2019-09-23 15:10:51 +0200

    Version 1.1.26 2019-09-23
    Improved implementation of DefaultOracleCustomMigrationProcessor

--- src/FluentDbTools/Implementations/FluentDbTools.Migration.Oracle/CustomProcessor/DefaultOracleCustomMigrationProcessor.cs
+++ src/FluentDbTools/Implementations/FluentDbTools.Migration.Oracle/CustomProcessor/DefaultOracleCustomMigrationProcessor.cs
@@ -9,74 +9,116 @@ using FluentDbTools.Migration.Abstractions.ExtendedExpressions;
 using FluentMigrator.Model;
 using FluentMigrator.Runner.Processors.Oracle;
 using Microsoft.Extensions.Logging;
+// ReSharper disable CommentTypo
 
 namespace FluentDbTools.Migration.Oracle.CustomProcessor
 {
     /// <inheritdoc />
     public class DefaultOracleCustomMigrationProcessor : ICustomMigrationProcessor<OracleProcessor>
     {
+        /// <summary>
+        /// IsNullable for "TIDSSTEMPEL" and "DIPS" collumn. Default is TRUE
+        /// </summary>
         [SuppressMessage("ReSharper", "IdentifierTypo")] 
         public virtual bool IsTidsStempelAndDipsIdIdNullable { get; set; } = true;
 
+        /// <summary>
+        /// The SchemaName of CommonSchema (Default "DIPSCOREDB")
+        /// </summary>
         [SuppressMessage("ReSharper", "StringLiteralTypo")] 
         public virtual string CommonSchemaName { get; set; } = "DIPSCOREDB";
 
+        /// <summary>
+        /// The SchemaName of AuthCodeSchema (Default "CORE_CODE")
+        /// </summary>
         public virtual string AuthCodeSchemaName { get; set; } = "CORE_CODE";
 
-        private readonly ILogger<DefaultOracleCustomMigrationProcessor> Logger;
-        private IExtendedMigrationProcessor Processor;
-        private Action<string> Execute;
+        /// <summary>
+        /// The Logger
+        /// </summary>
+        protected ILogger<DefaultOracleCustomMigrationProcessor> Logger { get; }
+
+        /// <summary>
+        /// The current IExtendedMigrationProcessorOracle 
+        /// </summary>
+        protected IExtendedMigrationProcessor Processor { get; set; }
+
+        /// <summary>
+        ///  Contains important Migration config values
+        /// </summary>
+        protected IDbMigrationConfig MigrationConfig { get;}
+
+        /// <summary>
+        /// Is Only enabled if DbType is Oracle
+        /// </summary>
+        protected bool Enabled { get; set; }
+
+        /// <summary>
+        /// The method for running Sql statemnsts
+        /// </summary>
+        protected Action<string> Execute { get; set; }
 
         /// <inheritdoc />
+        [SuppressMessage("ReSharper", "SuggestBaseTypeForParameter")]
         public DefaultOracleCustomMigrationProcessor(
             ILogger<DefaultOracleCustomMigrationProcessor> logger,
-            IExtendedMigrationProcessorOracle processor)
+            IExtendedMigrationProcessorOracle processor,
+            IDbMigrationConfig migrationConfig)
         {
             Logger = logger;
             Processor = processor;
+            MigrationConfig = migrationConfig;
+            Enabled = MigrationConfig.GetDbConfig().DbType == SupportedDatabaseTypes.Oracle;
         }
 
+        /// <inheritdoc />
         public virtual void MigrationMetadataChanged(IMigrationMetadata migrationMetadata, IExtendedMigrationProcessor extendedMigrationProcessor)
         {
             Processor = extendedMigrationProcessor;
-            if (Processor.SchemaExists(CommonSchemaName) && Processor.SchemaExists(AuthCodeSchemaName))
+            if (!Processor.SchemaExists(CommonSchemaName) || 
+                !Processor.SchemaExists(AuthCodeSchemaName))
             {
-                var logonSql = SqlResources.LogonScriptSql
-                                    .ReplaceIgnoreCase("{CommonSchemaName}", CommonSchemaName)
-                                    .ReplaceIgnoreCase("{AuthCodeSchemaName}", AuthCodeSchemaName)
-                                    .ReplaceIgnoreCase("{User}", migrationMetadata.MigrationName.SubstringTo("."));
-
-                Execute(logonSql);
+                return;
             }
+
+            var logonSql = SqlResources.LogonScriptSql
+                .ReplaceIgnoreCase("{CommonSchemaName}", CommonSchemaName)
+                .ReplaceIgnoreCase("{AuthCodeSchemaName}", AuthCodeSchemaName)
+                .ReplaceIgnoreCase("{User}", migrationMetadata.MigrationName.SubstringTo("."));
+
+            Execute(logonSql);
         }
 
+        /// <inheritdoc />
         public virtual void ConfigureSqlExecuteAction(Action<string> executeSqlAction)
         {
             Execute = executeSqlAction;
         }
 
+        /// <inheritdoc />
         public virtual void Process(IChangeLogTabledExpression expression)
         {
-            if (expression.ChangeLogContext != null)
+            if (expression.ChangeLogContext == null)
             {
-
-                Logger.LogInformation($"Process TableName: {expression.TableName}, " +
-                                      (expression.TableDescription != null ? $"TableDescription: {expression.TableDescription}, " : "") +
-                                      $"ShortName: {expression.ChangeLogContext.ShortName}, " +
-                                      $"GlobalId: {expression.ChangeLogContext.GlobalId}, " +
-                                      $"DbOperation: {expression.DbOperation}, " +
-                                      (expression.OldRenamedName != null ? $"OldRenamedName: {expression.OldRenamedName}" : ""));
-
-                if (expression.Columns.Any())
-                {
-                    foreach (var column in expression.Columns)
-                    {
-                        Logger.LogInformation($"Column:{column.Name} Table:{column.TableName}, Type: {column.Type}, Description: {column.ColumnDescription}");
-                    }
-                }
-
-                //Execute(string.Empty);
+                return;
             }
+
+            Logger.LogInformation($"Process TableName: {expression.TableName}, " +
+                                  (expression.TableDescription != null ? $"TableDescription: {expression.TableDescription}, " : "") +
+                                  $"ShortName: {expression.ChangeLogContext.ShortName}, " +
+                                  $"GlobalId: {expression.ChangeLogContext.GlobalId}, " +
+                                  $"DbOperation: {expression.DbOperation}, " +
+                                  (expression.OldRenamedName != null ? $"OldRenamedName: {expression.OldRenamedName}" : ""));
+
+            if (expression.Columns.Any())
+            {
+                foreach (var column in expression.Columns)
+                {
+                    Logger.LogInformation($"Column:{column.Name} Table:{column.TableName}, Type: {column.Type}, Description: {column.ColumnDescription}");
+                }
+            }
+
+            //Execute(string.Empty);
         }
 
         /// <summary>
@@ -87,18 +129,19 @@ namespace FluentDbTools.Migration.Oracle.CustomProcessor
         public virtual void ProcessAfter(ICreateSchemaWithPrefixExpression expression)
         {
             Logger.LogInformation($"ProcessAfter Schema:{expression.SchemaName} SchemaPrefix:{expression.SchemaPrefixId}");
-            if (Processor.SchemaExists(CommonSchemaName))
+            if (!Processor.SchemaExists(CommonSchemaName))
             {
-                var sqlAll = SqlResources.SchemaPrefixSql;
-
-                sqlAll = sqlAll.Replace("{CommonSchemaName}", CommonSchemaName);
-                sqlAll = sqlAll.Replace("{SchemaName}", expression.SchemaName.ToUpper());
-                sqlAll = sqlAll.Replace("{SchemaPrefixUniqueId}", expression.SchemaPrefixUniqueId);
-                sqlAll = sqlAll.Replace("{SchemaPrefixId}", expression.SchemaPrefixId);
-
-                Execute(sqlAll);
-
+                return;
             }
+
+            var sqlAll = SqlResources.SchemaPrefixSql;
+
+            sqlAll = sqlAll.Replace("{CommonSchemaName}", CommonSchemaName);
+            sqlAll = sqlAll.Replace("{SchemaName}", expression.SchemaName.ToUpper());
+            sqlAll = sqlAll.Replace("{SchemaPrefixUniqueId}", expression.SchemaPrefixUniqueId);
+            sqlAll = sqlAll.Replace("{SchemaPrefixId}", expression.SchemaPrefixId);
+
+            Execute(sqlAll);
         }
 
         /// <summary>
@@ -112,23 +155,26 @@ namespace FluentDbTools.Migration.Oracle.CustomProcessor
 
         }
 
+        /// <inheritdoc />
         public virtual string GenerateSql(ICreateSchemaWithPrefixExpression expression)
         {
-            if (Processor.SchemaExists(CommonSchemaName))
+            if (!Processor.SchemaExists(CommonSchemaName))
             {
-                var sqlAll = SqlResources.CreateSchemaSql;
-
-                sqlAll = sqlAll.Replace("{CommonSchemaName}", CommonSchemaName);
-                sqlAll = sqlAll.Replace("{SchemaName}", expression.SchemaName);
-                sqlAll = sqlAll.Replace("{SchemaPrefixUniqueId}", expression.SchemaPrefixUniqueId);
-                sqlAll = sqlAll.Replace("{SchemaPrefix}", expression.SchemaPrefixId);
-            
-                return sqlAll;
+                return null;
             }
 
-            return null;
+            var sqlAll = SqlResources.CreateSchemaSql;
+
+            sqlAll = sqlAll.Replace("{CommonSchemaName}", CommonSchemaName);
+            sqlAll = sqlAll.Replace("{SchemaName}", expression.SchemaName);
+            sqlAll = sqlAll.Replace("{SchemaPrefixUniqueId}", expression.SchemaPrefixUniqueId);
+            sqlAll = sqlAll.Replace("{SchemaPrefix}", expression.SchemaPrefixId);
+            
+            return sqlAll;
+
         }
 
+        /// <inheritdoc />
         public virtual IList<ColumnDefinition> GetDefaultColumns(string tableName = null)
         {
             if (Processor.SchemaExists(CommonSchemaName))
