ff963a80a8453077d5c639b50a72d21c4695bab0
Arild Håkon Olsen <aho@dips.no>
2019-10-24 23:45:28 +0200

    Version 1.1.29 2019-10-24
    - Improved LogFileAppendFluentMigratorLoggerProvider
      Will try 10 times with wait of 250ms to create StreamWriter if IOException is thrown

--- src/FluentDbTools/Implementations/FluentDbTools.Migration/LogFileAppendFluentMigratorLoggerProvider.cs
+++ src/FluentDbTools/Implementations/FluentDbTools.Migration/LogFileAppendFluentMigratorLoggerProvider.cs
@@ -1,6 +1,8 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using FluentMigrator.Runner.Initialization;
 using FluentMigrator.Runner.Logging;
 using Microsoft.Extensions.Options;
@@ -13,7 +15,8 @@ namespace FluentDbTools.Migration
     public class LogFileAppendFluentMigratorLoggerProvider : SqlScriptFluentMigratorLoggerProvider
     {
         /// <summary>
-        /// Initializes a new instance of the <see cref="LogFileAppendFluentMigratorLoggerProvider"/> class.
+        /// Initializes a new instance of the <see cref="LogFileAppendFluentMigratorLoggerProvider"/> class.<br/>
+        /// Will try 10 times with wait of 250ms to create StreamWriter if IOException is thrown 
         /// </summary>
         /// <param name="assemblySource">The assembly source</param>
         /// <param name="options">The log file logger options</param>
@@ -22,9 +25,37 @@ namespace FluentDbTools.Migration
                 IAssemblySource assemblySource,
                 IOptions<LogFileFluentMigratorLoggerOptions> options,
                 bool disposeWriter = true)
-            : base(new StreamWriter(GetOutputFileName(assemblySource, options.Value), true, Encoding.UTF8), options.Value, disposeWriter)
+            : base(GetStreamWriter(assemblySource, options), options.Value, disposeWriter)
         {
         }
+
+        private static StreamWriter GetStreamWriter(IAssemblySource assemblySource, IOptions<LogFileFluentMigratorLoggerOptions> options)
+        {
+            const int retryInitStreamWriter = 10;
+            Exception fileException = null;
+            var absolutePath = GetOutputFileName(assemblySource, options.Value);
+
+            for (var retries = 0; retries < retryInitStreamWriter; retries++)
+            {
+                try
+                {
+                    return new StreamWriter(absolutePath, true, Encoding.UTF8);
+                }
+                catch (IOException exception)
+                {
+                    fileException = exception;
+                    Task.Delay(TimeSpan.FromMilliseconds(250)).Wait();
+                }
+            }
+
+            if (fileException != null)
+            {
+                throw fileException;
+            }
+
+            return new StreamWriter(absolutePath, true, Encoding.UTF8);
+        }
+
         private static string GetOutputFileName(
             IAssemblySource assemblySource,
             LogFileFluentMigratorLoggerOptions options)
